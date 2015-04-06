009f62b64edd3268dbdd5592032499e4894654dc
BThacker <b.thacker@outlook.com>
2015-04-05 22:52:42 -0400

    Warmup2
    Revised

--- warmup2/WarmUp2.py
+++ warmup2/WarmUp2.py
@@ -2,7 +2,8 @@
 # www.codingbat.com PYTHON WarmUp1 Problems and Solutions 
 
 '''STRING_TIMES
-Given a string and a non-negative int n, return a larger string that is n copies of the original string. 
+Given a string and a non-negative int n, return a larger string 
+that is n copies of the original string. 
 
 string_times('Hi', 2) → 'HiHi'
 string_times('Hi', 3) → 'HiHiHi'
@@ -13,7 +14,10 @@ def string_times(str, n):
 	return str * n
 
 '''FRONT_TIMES
-Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front; 
+Given a string and a non-negative int n, we'll say that the 
+front of the string is the first 3 chars, or whatever is there 
+if the string is less than length 3. Return n copies of the 
+front; 
 
 front_times('Chocolate', 2) → 'ChoCho'
 front_times('Chocolate', 3) → 'ChoChoCho'
@@ -29,7 +33,8 @@ def front_times(str, n):
 	return sub * n
 
 '''STRING_BITS
-Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+Given a string, return a new string made of every other char 
+starting with the first, so "Hello" yields "Hlo".
 
 string_bits('Hello') → 'Hlo'
 string_bits('Hi') → 'H'
@@ -47,7 +52,8 @@ def string_bits(str):
 	return newString
 
 '''STRING_SPLOSION
-Given a non-empty string like "Code" return a string like "CCoCodCode". 
+Given a non-empty string like "Code" return a string like 
+"CCoCodCode". 
 
 string_splosion('Code') → 'CCoCodCode'
 string_splosion('abc') → 'aababc'
@@ -64,7 +70,10 @@ def string_splosion(str):
 	return newString
 
 '''LAST2
-Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring). 
+Given a string, return the count of the number of times that 
+a substring length 2 appears in the string and also as the 
+last 2 chars of the string, so "hixxxhi" yields 1 (we won't 
+	count the end substring). 
 
 last2('hixxhi') → 1
 last2('xaxxaxaxx') → 1
@@ -83,7 +92,8 @@ def last2(str):
 
 '''ARRAY_COUNT9
 
-Given an array of ints, return the number of 9's in the array. 
+Given an array of ints, return the number of 9's in the 
+array. 
 
 array_count9([1, 2, 9]) → 1
 array_count9([1, 9, 9]) → 2
@@ -100,7 +110,9 @@ def array_count9(nums):
 	return counter
 
 '''ARRAY_FRONT9
-Given an array of ints, return True if one of the first 4 elements in the array is a 9. The array length may be less than 4. 
+Given an array of ints, return True if one of the first 4 
+elements in the array is a 9. The array length may be less 
+than 4. 
 
 array_front9([1, 2, 9, 3, 4]) → True
 array_front9([1, 2, 3, 4, 9]) → False
@@ -118,7 +130,8 @@ def array_front9(nums):
 	return False
 
 '''ARRAY123
-Given an array of ints, return True if .. 1, 2, 3, .. appears in the array somewhere. 
+Given an array of ints, return True if .. 1, 2, 3, .. appears 
+in the array somewhere. 
 
 array123([1, 1, 2, 3, 1]) → True
 array123([1, 1, 2, 4, 1]) → False
@@ -132,7 +145,10 @@ def array123(nums):
 	return False
 
 '''STRING_MATCH
-Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings. 
+Given 2 strings, a and b, return the number of the positions 
+where they contain the same length 2 substring. So "xxcaazz" 
+and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings 
+ppear in the same place in both strings. 
 
 string_match('xxcaazz', 'xxbaaz') → 3
 string_match('abc', 'abc') → 2
